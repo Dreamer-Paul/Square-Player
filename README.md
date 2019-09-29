a88b0b23af10162bfd2afd8d21b73f458438fda0
奇趣保罗 <dreamer_paul@126.com>
2019-09-29 23:35:53 +0800

    Update Function
    优化标签名称，修复 API 问题，增加去重处理

--- README.md
+++ README.md
@@ -6,12 +6,12 @@
 
 1. `Star` 本项目
 2. 从这里 [下载](https://github.com/Dreamer-Paul/Square-Player/archive/master.zip) 源码
-3. 可使用本地、网易云音乐两种方式食用，参照下面的快速食用指南即可快速部署完成
+3. 可使用本地、网易云音乐两种方式食用，参照 [文档站](https://docs.paul.ren/square) 即可快速部署完成
 4. 本项目提供 Typecho 插件版本，如需使用请先将下载后得到的文件夹从 `Square-Player-master` 重命名为 `SQP`
 
 ## 开源协议
 
-本项目采用 MIT 开源协议进行授权，须保留原作者的版权注释（CSS、JS 文件）
+本项目采用 MIT 开源协议进行授权，请保留原作者的版权注释（CSS、JS 文件）
 
 原创不易！如果喜欢本项目，请 `Star` 它以示对我的支持~
 
@@ -19,66 +19,6 @@
 
 ## 感谢
 
-感谢来自开源社区提供的解决方案，如果没有它们，Square Player 可能还无法如此完善 ヽ(*≧ω≦)ﾉ
+感谢来自开源社区提供的解决方案，如果没有它们，Square Player 可能还无法如此完善~
 
  - [Meting](https://github.com/metowolf/Meting)
-
-## 快速食用指南
-
-在网页 `head` 标签引用项目的 CSS 文件 `SQPlayer.css`：
-
-```html
-<head>
-...
-<link href="SQPlayer.css" rel="stylesheet" type="text/css"/>
-...
-</head>
-```
-
-在网页 `body` 标签内的最后一行引用项目的 JS 文件 `SQPlayer.js`：
-
-```html
-<body>
-...
-<script src="SQPlayer.js"></script>
-</body>
-```
-
-在网页 `body` 标签内插入一个 `sqplayer` 标签：
-
-```html
-<body>
-...
-<sqplayer></sqplayer>
-...
-</body>
-```
-
-Square Player 支持以下属性，他们分别的意义是：
-
-属性名|用途
-:-:|:-:
-right|至右显示
-data-title|歌曲名
-data-artist|艺术家
-data-cover|专辑封面图片链接
-data-link|歌曲地址
-data-163|网易云音乐的 ID 或歌名
-
-如果使用 `data-163` 属性引用播放器，就写成这样，其中 `23682511` 就是一首歌的 ID。
-
-```html
-<sqplayer data-163="23682511"></sqplayer>
-```
-
-如果使用静态方法引用播放器，就需要同时编写四个属性。
-
-```html
-<sqplayer right data-title="Crimson & Clover" data-artist="Tommy James" data-cover="封面链接" data-link="歌曲链接"></sqplayer>
-```
-
-Enjoy it！
-
-# Typecho 插件版
-
-按照如上的配置直接粘贴在文章里即可使用
--- SQPlayer.js
+++ SQPlayer.js
@@ -4,7 +4,7 @@
 
 # Square Player
 # By: Dreamer-Paul
-# Last Update: 2019.5.9
+# Last Update: 2019.9.29
 
 一个简洁到极致的单曲播放器。
 
@@ -12,19 +12,22 @@
 
 ---- */
 
+let _Players = [];
+
 class SQPlayer{
-    constructor(wrapper){
+    constructor(wrapper, key, set){
+        this.key = key;
         this.elements = {
             wrap: wrapper,
             player: this.creator("audio"),
-            info: this.creator("div", "info"),
-            title: this.creator("span", "title"),
-            toggle: this.creator("div", "toggle")
+            info: this.creator("div", {cls: "info"}),
+            title: this.creator("span", {cls: "title"}),
+            toggle: this.creator("div", {cls: "toggle"})
         };
 
         this.elements.player.setAttribute("preload", "none");
-
-        wrapper.dataset[163] ? this.getBy163(wrapper) : this.setup(wrapper.dataset);
+        _Players.push(this);
+        wrapper.dataset[163] ? this.getBy163(wrapper, set.server) : this.setup(wrapper.dataset);
     }
 
     // 切换按钮
@@ -34,20 +37,31 @@ class SQPlayer{
 
     play(){
         this.elements.player.play();
+
+        _Players.forEach((item) => {
+            if(item !== this) item.pause();
+        })
     }
 
     pause(){
         this.elements.player.pause();
     }
 
-    creator(tag, cls){
+    creator(tag, attr){
         let a = document.createElement(tag);
-        if(cls) a.className = "sqp-" + cls;
+
+        if(attr){
+            if(attr.cls) a.className = "sqp-" + attr.cls;
+            if(attr.content) a.innerHTML = attr.content;
+        }
+
         return a;
     }
 
     // 设置播放器
     setup(item){
+        const fontSize = window.getComputedStyle(document.querySelector("html")).fontSize.replace("px", "");
+
         item.link ? this.elements.player.src = item.link : console.error("No files to play!");
 
         if(item.cover){
@@ -57,13 +71,35 @@ class SQPlayer{
         // 播放器主体初始化
         this.elements.title.innerText = item.artist && item.title ? item.title + " - " + item.artist : "未知标题";
 
-        this.elements.toggle.addEventListener("click", () => {
-            this.toggle();
-        });
-
         this.elements.info.appendChild(this.elements.title);
         this.elements.wrap.appendChild(this.elements.info);
         this.elements.wrap.appendChild(this.elements.toggle);
+
+        let offset = this.elements.title.offsetWidth - (fontSize * 8);
+        let time = parseInt(this.elements.title.offsetWidth / 30);
+
+        const ani = this.creator("style", {content: `
+@keyframes sqp-title-${this.key} {
+    0%{
+        transform: translateX(0);
+    }
+    50%{
+        transform: translateX(${-offset}px);
+    }
+    100%{
+       transform: translateX(0);
+    }
+}
+`});
+
+        if(offset > 0){
+            this.elements.title.style.animation = "sqp-title-" + this.key + " " + time + "s infinite linear";
+            this.elements.wrap.appendChild(ani);
+        }
+
+        this.elements.toggle.addEventListener("click", () => {
+            this.toggle();
+        });
 
         this.elements.player.addEventListener("playing", () => {
             this.elements.toggle.classList.add("pause");
@@ -75,51 +111,54 @@ class SQPlayer{
 
     getBy163(value, server){
         let id = value.getAttribute("data-163");
-        let type = isNaN(value) ? "?title=" : "?id=";
 
-        if(server === "paul"){
-            fetch("https://api.paugram.com/netease/" + type + id).then(function (res) {
-                return res.json();
-            }).then((item) => {
-                this.setup(item);
-            });
-        }
-        else{
-            type = isNaN(value) ? `type=song&id=${id}` : `type=search&id=${id}`;
+        const s = {
+            "meto": () => {
+                fetch(`https://api.i-meto.com/meting/api?server=netease&id=${id}`).then((res) => {
+                    return res.json();
+                }).then((item) => {
+                    item = item[0];
 
-            fetch(`https://api.i-meto.com/api/v1/meting?server=netease&${type}`).then((res) => {
-                return res.json();
-            }).then((i) => {
-                i = i[0];
+                    this.setup({
+                        title:  item.name,
+                        artist: item.artist,
+                        cover:  item.cover,
+                        link:   item.url,
+                    });
+                })
+            },
+            "paul": () => {
+                const type = isNaN(value) ? "?title=" : "?id=";
 
-                this.setup({
-                    title:  i.name,
-                    artist: i.artist,
-                    cover:  i.cover,
-                    link:   i.url,
+                fetch("https://api.paugram.com/netease/" + type + id).then(function (res) {
+                    return res.json();
+                }).then((item) => {
+                    this.setup(item);
                 });
-            })
+            }
         }
+
+        s[server]();
     }
 }
 
 console.log("%c Square Player %c https://paugram.com ","color: #fff; margin: 1em 0; padding: 5px 0; background: #1875b3;","margin: 1em 0; padding: 5px 0; background: #efefef;");
 
 class SQP_Extend {
-    constructor(wrapper){
-        this.init(wrapper);
+    constructor(settings){
+        this.init(settings);
     }
 
-    init(wrapper){
+    init(settings){
         this.players = [];
-        wrapper = document.querySelectorAll(wrapper);
+        this.wrapper = document.querySelectorAll("sqp");
 
-        wrapper.forEach(function (item) {
-            this.players.push(new SQPlayer(item));
+        this.wrapper.forEach(function (item, key) {
+            this.players.push(new SQPlayer(item, key, settings));
         }, this)
     }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    let SQP_EX = new SQP_Extend("sqplayer");
+    let SQP_EX = new SQP_Extend({server: "meto"});
 });
